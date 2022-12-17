b62fc4b823e7f10d83eb3316905972df2735d4d2
Sherzod Abdullaev <perfekt.vision2019@gmail.com>
2022-12-17 13:05:19 +0100

    deploying to netlify

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
